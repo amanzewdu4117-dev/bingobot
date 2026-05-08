628fce0d01bf47542343e260367779e8130c2ed1
amanzewdu4117-dev <amanzewdu4117@gmail.com>
2026-05-08 12:15:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import telebot
+from telebot import types
+
+TOKEN = "የአንተ_ቦት_ቶከን_እዚህ_ይግባ"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.InlineKeyboardMarkup()
+    # url የሚለው ቦታ ላይ በኋላ Netlify ላይ የምናገኘውን ሊንክ እናስገባለን
+    web_info = types.WebAppInfo(url="https://your-app-link.netlify.app")
+    button = types.InlineKeyboardButton(text="ቢንጎ ተጫወት 🎮", web_app=web_info)
+    markup.add(button)
+    
+    bot.send_message(message.chat.id, "እንኳን ደህና መጡ! ጨዋታውን ለመጀመር ከታች ያለውን በተን ይጫኑ።", reply_markup=markup)
+
+print("ቦቱ እየሰራ ነው...")
+bot.polling()
